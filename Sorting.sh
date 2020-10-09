07cdafabd63221607783d74517cc9f3dd26ca66f
saurav004 <logtosaurav@gmail.com>
2020-10-09 22:21:56 +0530

    ADDED:Stored Results in a dictionary

--- Sorting.sh
+++ Sorting.sh
@@ -5,12 +5,16 @@ echo "---------------------------------------------------------------Welcome to 
 read -p "Enter a number" a
 read -p "Enter a number" b
 read -p "Enter a number" c
+declare -A DICT
+
 echo "1st Number=$a, 2nd Number=$b, 3rd Number=$c"
 
 #Calculating  Expressions 1.$a+$b*$c 2.$a*$b+$c 3.$c+$a/$b 4.$a%$b+$c)
 
-result1=$(($a+$b*$c))
-result2=$(($a*$b+$c))
-result3=$(($c+$a/$b))
-result4=$(($a%$b+$c))
-echo $result1 $result2 $result3 $result4
+#ADDING RESULTS IN A DICTIONARY
+
+DICT[1]=$(($a+$b*$c))
+DICT[2]=$(($a*$b+$c))
+DICT[3]=$(($c+$a/$b))
+DICT[4]=$(($a%$b+$c))
+echo  ${DICT[@]}
