c0df2948294a457dd95c881d39ea86869000ef72
saurav004 <logtosaurav@gmail.com>
2020-10-09 21:43:34 +0530

    Added:code to read 3 numbers from command line

--- Sorting.sh
+++ Sorting.sh
@@ -1,3 +1,8 @@
 #! /bin/bash -x
 
 echo "---------------------------------------------------------------Welcome to SORTING AND ARITHMETIC COMPUTATION----------------------------------------------------------------------"
+#reading 3 inputs from Command Line
+read -p "Enter a number" a
+read -p "Enter a number" b
+read -p "Enter a number" c
+echo "1st Number=$a, 2nd Number=$b, 3rd Number=$c"
